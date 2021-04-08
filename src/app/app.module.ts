1b4f365bd88ae5905a0aec7458cad738c5574c55
c-manna <cmanna7@gmail.com>
2021-04-08 11:49:43 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { OnlyNumberDirective } from './only-number.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, OnlyNumberDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
