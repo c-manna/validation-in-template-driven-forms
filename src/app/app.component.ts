8f0e10425a3c3a6c94a190f071572ae36a852a31
c-manna <cmanna7@gmail.com>
2021-04-08 10:17:43 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component, VERSION } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+  mobNumberPattern = "^((\\+91-?)|0)?[0-9]{10}$";  
+ isValidFormSubmitted = false;  
+ user = new User();  
+
+
+ onFormSubmit(form: NgForm) {  
+   this.isValidFormSubmitted = false;  
+   if (form.invalid) {  
+      return;  
+   }  
+   this.isValidFormSubmitted = true;  
+   form.resetForm();  
+}  
+}  
+
+export class User {  
+ mobileNumber ?: string;  
+}  
